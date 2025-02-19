a6e0a29bd3c68f2d63f82d454f3f4fe781cd773f
DevCheckOG <ing.kevin.benavides@gmail.com>
2025-02-19 15:45:26 -0600

    Fixed unexpected bad syntax.

--- src/cli.rs
+++ src/cli.rs
@@ -311,7 +311,12 @@ impl Cli {
     fn help(&self) {
         println!(
             "\n{}\n",
-            style("The Thrush Compiler")et _ =
+            style("The Thrush Compiler")
+                .bold()
+                .fg(Color::Rgb(141, 141, 142))
+        );
+
+        println!(
             "{} {} {}\n",
             style("Usage:").bold(),
             style("thrushc").bold().fg(Color::Rgb(141, 141, 142)),
