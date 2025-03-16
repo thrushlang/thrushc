358a9507faf63d78d65f67e48fcacc3a80ef0c9f
Kevin Benavides <126353237+DevCheckOG@users.noreply.github.com>
2025-03-16 10:27:58 -0600

    `rsync` is better than `cp`.

--- build.py
+++ build.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         
         for index, source in enumerate(files):
             for file in source:
-                os.system(f"cp {file} /usr/include/") if index == 0 else os.system(f"sudo cp {file} /usr/lib/") if index == 1 else os.system(f"sudo cp {file} /usr/bin/")
+                os.system(f"rsync -r -a --mkpath {file} /usr/include/") if index == 0 else os.system(f"rsync -r -a --mkpath {file} /usr/lib/") if index == 1 else os.system(f"rsync -r -a --mkpath {file} /usr/bin/")
 
     def build_dependencies_for_linux(): 
 
