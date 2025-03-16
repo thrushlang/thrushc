498a3f7732de6491c3c565fcc7c36aafddc7b4d2
DevCheckOG <ing.kevin.benavides@gmail.com>
2025-03-16 10:57:34 -0600

    Fixed errors in build.py when copying files.

--- build.py
+++ build.py
@@ -13,33 +13,6 @@ if __name__ == "__main__":
 
     elevate.elevate()
 
-    def get_files(path: str, generated_target: List[str], visited: set = None) -> List[str]:
-        if not os.path.exists(path):
-            return generated_target
-
-        if visited is None:
-            visited = set()
-
-        if path in visited:
-            return generated_target
-
-        visited.add(path)
-
-        for name in os.listdir(path):
-            full_path = os.path.join(path, name)
-            if os.path.isfile(full_path):
-                generated_target.append(os.path.abspath(full_path))
-            elif os.path.isdir(full_path):
-                get_files(full_path, generated_target, visited)
-
-        return generated_target
-
-    def install_linux_llvm_for_thrushc(files: List[List[str]]): 
-        
-        for index, source in enumerate(files):
-            for file in source:
-                os.system(f"rsync -r -a --mkpath {file} /usr/include/") if index == 0 else os.system(f"rsync -r -a --mkpath {file} /usr/lib/") if index == 1 else os.system(f"rsync -r -a --mkpath {file} /usr/bin/")
-
     def build_dependencies_for_linux(): 
 
         print("Building dependencies for the Thrush compiler in Linux...")
@@ -50,11 +23,7 @@ if __name__ == "__main__":
         print("Unpacking the precompiled LLVM-C API v17.0.6...")
         tar: int = os.system("tar xvf thrushc-llvm-linux-x64_86-v1.0.0.tar.gz")
 
-        llvm_c_includes: List[str] = get_files("llvm/include")
-        llvm_c_libraries: List[str] = get_files("llvm/lib")
-        llvm_c_binaries: List[str] = get_files("llvm/bin")
-
-        install_linux_llvm_for_thrushc([llvm_c_includes, llvm_c_libraries, llvm_c_binaries])
+        os.system(f"rsync -r -a -mkpath {os.path.abspath("llvm/")} /usr/")
 
         if sum([wget, tar]) > 0:
             print("Failed to install LLVM-C API v17.0.6.")
