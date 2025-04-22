8f1b263c8f6b5b280a941fc89dcaef78f186fc39
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-04-21 21:42:15 -0600

    Refact and fix

--- build.py
+++ build.py
@@ -6,29 +6,39 @@ import shutil
 
 if __name__ == "__main__":
     SYSTEM: str = platform.system().lower()
-    
+
     HOME: str = os.environ["HOME"] if SYSTEM == "linux" else os.environ["APPDATA"].replace("\\", "/")
 
-    os.makedirs(f"{HOME}/thrushlang/backends/llvm/build", exist_ok= True)
+    BUILD_PATH: str = os.path.join(HOME, "thrushlang", "backends", "llvm", "build")
 
-    def build_dependencies_for_linux(): 
+    os.makedirs(BUILD_PATH, exist_ok= True)
+
+    def build_dependencies_for_linux():
+        COMPRESS_FILE: str = "thrushc-llvm-linux-x64-v1.0.0.tar.gz"
+        GITHUB_BUILD_FILE: str = f"https://github.com/thrushlang/toolchains/releases/download/LLVM-C/{COMPRESS_FILE}"
+
+        if os.path.exists(COMPRESS_FILE):
+            print(f"{COMPRESS_FILE} already exists. Deleting...")
+            os.remove(COMPRESS_FILE)
 
         print("Building dependencies for The Thrush Compiler in Linux...")
 
-        if not os.path.exists("thrushc-build"): os.mkdir("thrushc-build")
+        os.makedirs("thrushc-build", exist_ok=True)
+        os.makedirs(os.path.abspath('llvm/'), exist_ok=True)
 
-        wget: int = os.system("wget https://github.com/thrushlang/toolchains/releases/download/LLVM-C/thrushc-llvm-linux-x64-v1.0.0.tar.gz -o thrushc-build/thrushc-llvm-linux-x64-v1.0.0.tar.gz")
-        tar: int = os.system("tar xvf thrushc-build/thrushc-llvm-linux-x64-v1.0.0.tar.gz")
-        decompress: int = os.system(f"rsync -r -a -mkpath {os.path.abspath("llvm/")} {HOME}//thrushlang/backends/llvm/build")
+        wget: int = os.system(f"wget {GITHUB_BUILD_FILE} -O {os.path.join('thrushc-build', COMPRESS_FILE)} -o /dev/null")
+        gunzip: int = os.system(f"gunzip -c {os.path.join('thrushc-build', COMPRESS_FILE)} > {os.path.join('thrushc-build', 'llvm.tar')}")
+        decompress: int = os.system(f"tar -xvf thrushc-build/llvm.tar -C {BUILD_PATH}")
 
-        if sum([wget, tar, decompress]) > 0:
-            print("Failed to install LLVM-C API.")
-            sys.exit(1)
+        for action in [wget, gunzip, decompress]:
+            if action != 0:
+                print(f"Error in {action}")
+                sys.exit(1)
 
         shutil.rmtree("thrushc-build/", ignore_errors= True)
 
         print("Dependencies are ready to compile. Use 'cargo clean' and 'cargo run' now.")
-        
+
         sys.exit(0)
 
     def build_dependencies_for_windows():
@@ -39,7 +49,7 @@ if __name__ == "__main__":
 
         urllib.request.urlretrieve("https://github.com/thrushlang/toolchains/releases/download/LLVM-C/thrushc-llvm-windows-x64-v1.0.0.tar.xz", "thrushc-build/thrushc-llvm-windows-x64-v1.0.0.tar.xz")
 
-        tar: int = os.system(f"tar -xJf thrushc-build/thrushc-llvm-windows-x64-v1.0.0.tar.xz -C {HOME}/thrushlang/backends/llvm/build/") 
+        tar: int = os.system(f"tar -xJf thrushc-build/thrushc-llvm-windows-x64-v1.0.0.tar.xz -C {HOME}/thrushlang/backends/llvm/build/")
 
         if tar > 0:
             print("Failed to install the LLVM-C API.")
@@ -54,7 +64,7 @@ if __name__ == "__main__":
     match SYSTEM:
         case "linux":
             build_dependencies_for_linux()
-        case "windows": 
+        case "windows":
             build_dependencies_for_windows()
 
     print(f"Usage: {"py" if SYSTEM == "windows" else "python3"}")
