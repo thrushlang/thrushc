43e8acce7ab3679242207d58889edc07fac9046b
Kevin Benavides <126353237+DevCheckOG@users.noreply.github.com>
2025-08-14 21:29:32 -0600

    `FIX` Pop up warning in rustc compiletime and building.

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 fn main() {
     if cfg!(target_os = "linux") {
-        println!("cargo:warning=yeeee");
         println!("cargo:rustc-linker=clang");
         println!("cargo:rustc-link-arg=-fuse-ld=lld");
     }
